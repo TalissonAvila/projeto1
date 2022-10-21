1cffe89f3c78656469d8bfc6d40be5f0a65c36a9
Talisson Avila <talisson_avila@hotmail.com>
2022-10-21 16:58:20 -0300

    add docString

--- client/client/client/client.py
+++ client/client/client/client.py
@@ -1,31 +1,91 @@
 # client side
+
+from typing import BinaryIO
 from socket import *
 
 
-def send_file(path: str):
-    """It receives the file inside its folders, and initiates a connection with the server, sending this file in
-     a size of 1024 bytes.
+class Client:
+    def __init__(self):
+        """Construct"""
+        self.host = gethostname()
+        self.port = 55551
+        self.client = socket(AF_INET, SOCK_STREAM)
 
-    Args:
-        path (str) : Path/file.extension.
+    def client_connection(self):
+        """Connects the client using host by 'gethostname()' method and port by port 55551.
 
-    Return:
-        None.
-    """
-    host = gethostname()
-    port = 55551
-    client = socket(AF_INET, SOCK_STREAM)
-    client.connect((host, port))
-    file_path_client_side = open(path, "rb")
-    read_buffer = file_path_client_side.read()
+        Args:
+            None.
 
-    while read_buffer:
-        client.send(read_buffer)
-        read_buffer = file_path_client_side.read(1024)
-        print('sending file...')
-    client.close()
-    print('file sent.')
+        Returns:
+            None.
+        """
+        self.client.connect((self.host, self.port))
+
+    @staticmethod
+    def open_file(path: str) -> BinaryIO:
+        """Receive the path where the file is located and returns the file opened.
+
+        Args:
+            path(str): the Path/file.exe where the file is located.
+
+        Returns:
+            the file opened.
+        """
+        file_path_client_side = open(path, "rb")
+        return file_path_client_side
+
+    @staticmethod
+    def read_file(file_open: BinaryIO) -> bytes:
+        """Receive the file open and reads it into a variable in a form of bytes.
+
+        Args:
+            file_open (BinaryIO): the opened binary file.
+
+        Returns:
+            the file open in a form of bytes.
+        """
+        read_buffer = file_open.read()
+        return read_buffer
+
+    def send_file(self, read_buffer: bytes, file_opened: BinaryIO):
+        """Receives the file open in a form of bytes and sends to server with length of 1024 bytes.
+
+        Args:
+            read_buffer (bytes): the file divided with length of 1024 bytes.
+            file_opened (BinaryIO): the opened binary file.
+
+        Returns:
+             None.
+        """
+        while read_buffer:
+            self.client.send(read_buffer)
+            read_buffer = file_opened.read(1024)
+            print('sending file...')
+
+    def close_client(self):
+        """After finishing uploading the file, closes the client-side connection.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.client.close()
+        print('file sent.')
+
+    def main(self):
+        """Main function that receives the location of the file, establish a connection, open the file in a form of
+         bytes and sends to server.
+        """
+        file_path = str("C:/!!Coisas Importantes!!/song1.mp3")
+        Client.client_connection(self)
+        open_file = Client.open_file(file_path)
+        read_buffer = Client.read_file(open_file)
+        Client.send_file(self, read_buffer, open_file)
+        Client.close_client(self)
 
 
-file_path = str("C:/!!Coisas Importantes!!/song1.mp3")
-send_file(file_path)
+client = Client()
+client.main()
--- server/server/server/server.py
+++ server/server/server/server.py
@@ -1,42 +1,106 @@
 # server side
+
+from typing import BinaryIO
 from socket import *
 
 
-def recieve_file():
-    """Opens a connection and waits to receive file. When establishing the connection, it receives the file divided into
-    sizes of 1024 bytes. After receiving it complete, it closes the connection and the server.
+class Server:
+    def __init__(self):
+        """Construct"""
+        self.host = gethostname()
+        self.port = 55551
+        self.server = socket(AF_INET, SOCK_STREAM)
 
-    Args:
-        None.
+    def server_connection(self):
+        """Binds socket to address.Then shows IP and port on terminal.
 
-    Return:
-        None.
-    """
-    host = gethostname()
-    port = 55551
-    print('Runing server.')
-    print(f'HOST: {host} | PORT: {port}')
-    server = socket(AF_INET, SOCK_STREAM)
-    server.bind((host, port))
-    server.listen(5)
-    running = 1
-    while running == 1:
-        fd, address = server.accept()
+        Args:
+            None.
+        Returns:
+            None.
+        """
+        self.server.bind((self.host, self.port))
+        print(f'HOST: {self.host} | PORT: {self.port}')
+
+    def listened_time(self):
+        """Specifies the number of unaccepted connections that the server will allow, five,  before refusing
+         new connections.
+
+        Returns:
+            None.
+
+        Args:
+            None.
+        """
+        self.server.listen(5)
+
+    def running_connection(self, file_path: BinaryIO):
+        """Accepts the connection from client and writes the file with length of 1024 bytes. After completed,
+        closes the server connection, show's it on terminal.
+
+        Args:
+            file_path (BinaryIO): receive the location where the file it will be on server.
+
+        Returns:
+            None.
+        """
+        connection, address = self.server.accept()
+        read_buffer = connection.recv(1024)
+        while read_buffer:
+            file_path.write(read_buffer)
+            read_buffer = connection.recv(1024)
+        connection.close()
+        print('Connection closed.')
+
+    @staticmethod
+    def close_file(file_path: BinaryIO):
+        """Closes the file connection and shows it on terminal that the file was received.
+
+        Args:
+            file_path (BinaryIO):
+
+        Returns:
+            None.
+        """
+        file_path.close()
+        print('file received.')
+
+    @staticmethod
+    def file_path_server() -> BinaryIO:
+        """Chooses the file path on server side and opens, then prepare to write.
+
+        Args:
+            None.
+
+        Returns:
+            file_path (BinaryIO): the file location on server side.
+        """
         file_path = open("uploads/file_rcv.mp3", 'wb')
-        end = 0
-        while end == 0:
-            read_buffer = fd.recv(1024)
-            while read_buffer:
-                file_path.write(read_buffer)
-                read_buffer = fd.recv(1024)
-                end = 1
-            file_path.close()
-            print('File recieved.')
-            fd.close()
-            running = 0
-            print('Connection closed.')
-    server.close()
-    print('Server closed.')
+        return file_path
+
+    @staticmethod
+    def close_server(self):
+        """Closes the server connection then shows it on terminal.
+
+        Args:
+            None.
+        Returns:
+            None.
+        """
+        self.server.close()
+        print('Server closed.')
+
+    def main(self):
+        """Main function that open connections, that defines five as listening time. Receives the file from client then
+        saves on an 'uploads' folder. Then closes: connection, file and server.
+        """
+        Server.server_connection(self)
+        Server.listened_time(self)
+        file = Server.file_path_server()
+        Server.running_connection(self, file)
+        Server.close_file(file)
+        Server.close_server(self)
 
 
-recieve_file()
+server = Server()
+server.main()
